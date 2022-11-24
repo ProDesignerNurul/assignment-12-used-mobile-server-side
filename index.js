5c583de4fe4ae0e4abe72cb12ffa3d7f2943102f
ProDesignerNurul <maxnewsbd@gmail.com>
2022-11-24 20:15:45 +0600

    added some data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,37 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+// db codeed 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zwnyjff.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const usedMobileCollection = client.db('usedMobileResaler').collection('usedMobiles');
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const mobileCategory = await usedMobileCollection.find( query ).toArray();
+            res.send(mobileCategory);
+        });
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const allMobiles = await usedMobileCollection.find(query).toArray();
+            res.send(allMobiles);
+        });
+
+    }
+
+    finally{
+
+    }
+};
+run().catch(err => console.error(err));
 
 
 
